41a97bbc710cd153d2eda1f447b1b1fe65b97624
Rafael Zimmer <rzimmerdev@gmail.com>
2024-04-25 22:26:44 -0300

    Working surface viewer
    # TODO: Update filter

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,6 +1,9 @@
 import os
 import zipfile
+
+import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 
 from torch.utils.data import Dataset
 
@@ -11,7 +14,7 @@ class VolatilityDataset(Dataset):
         self.path = path
         self.file = file
 
-        self.data = None
+        self.data: pd.DataFrame = pd.DataFrame()
 
     def load(self, file=None):
         if not os.path.exists(self.path):
@@ -23,19 +26,73 @@ class VolatilityDataset(Dataset):
                 zip_ref.extractall(self.path)
 
         self.data = pd.read_csv(f"{self.path}/{file}")
+        self.preprocess()
+
+    def preprocess(self):
+        # divide daysToExpiration by calendar days
+        self.data["maturity"] = self.data["daysToExpiration"] / 360
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        # since talking about option volatility surfaces, get all points that have the same maturity
-        # and the same strike price, so make permutations of unique values of maturity and strike price
-        # strikes = self.data["strike"].unique()
-        # maturities = self.data["maturity"].unique()
         return self.data.iloc[idx]
+
+    def get(self, maturity: tuple, strike: tuple, date: str):
+        """
+        [5 rows x 34 columns]
+        Index(['14d', '30d', '3d', '5d', '60d', '7d', 'ask', 'bid', 'bs',
+       'daysToExpiration', 'delta', 'dist', 'dist_pct', 'dt', 'expr',
+       'extrinsic', 'extrinsic_pct', 'gamma', 'inTheMoney', 'intrinsic',
+       'intrinsic_pct', 'iv', 'mark', 'openInterest', 'rho', 'strike',
+       'theoreticalOptionValue', 'theoreticalVolatility', 'theta', 'timeValue',
+       'underlying', 'vega', 'volatility', 'prev_iv'],
+      dtype='object')
+        :param maturity: interval of maturity
+        :param strike: interval of strike
+        :return:
+        """
+        # return self.data[
+        #     (self.data["maturity"] >= maturity[0]) & (self.data["maturity"] <= maturity[1]) &
+        #     (self.data["strike"] >= strike[0]) & (self.data["strike"] <= strike[1])]
+        return self.data[
+            (self.data["maturity"] >= maturity[0]) & (self.data["maturity"] <= maturity[1]) &
+            (self.data["strike"] >= strike[0]) & (self.data["strike"] <= strike[1]) &
+            (self.data["dt"] == date)]
+
+
+class Dataviewer:
+    def __init__(self):
+        pass
+
+    def view_surface(self, df):
+        df = df.dropna().drop_duplicates()
+        x = df["strike"].values
+        y = df["maturity"].values
+
+        # tile z
+        z = df["iv"].values
+        z = np.tile(z, (len(z), 1))
+
+        fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
+        fig.update_layout(title='IV Surface',
+                          scene=dict(
+                              xaxis_title='Strike',
+                              yaxis_title='Maturity',
+                              zaxis_title='IV'),
+                          autosize=False,
+                          width=800, height=800)
+
+        fig.show()
 
 
 if __name__ == "__main__":
     dataset = VolatilityDataset()
     dataset.load("option_SPY_dataset_combined.csv")
     print(dataset.data.head())
+    print(dataset.data.columns)
+
+    data = dataset.get((0.0, 0.2), (300, 400), "2021-01-04")
+
+    viewer = Dataviewer()
+    viewer.view_surface(data)
