39ab7857e47c6eb77f6258a5ae09b7b8ee4b11c6
Rafael Zimmer <rzimmerdev@gmail.com>
2024-05-04 00:02:20 -0300

    Added SABR previewer

--- src/sabr.py
+++ src/sabr.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 
 class SABR:
     def __init__(self, a, b, r):
@@ -22,41 +24,56 @@ class SABR:
         X = math.log((math.sqrt(1 - 2 * self.r * z + z ** 2) + z - self.r) / (1 - self.r))
         return self.a / (S * K) ** ((1 - self.b) / 2) * (1 + (1 - self.b) ** 2 / 24 * z ** 2 + (1 - self.b) ** 4 / 1920 * z ** 4) * T
 
+    def preview(self, ax):
+        S = 100
+        T = np.linspace(0.1, 2, 10)
+        K = np.array([
+            np.random.randint(80, 120, 100) for _ in T
+        ])
+
+        IV = np.array([
+            [self.ivol(S, k, t) for k in K[i]] for i, t in enumerate(T)
+        ])
+
+        # flatten K and IV, and make T the same size
+        T = np.array([
+            [t] * len(K[i]) for i, t in enumerate(T)
+        ]).flatten()
+
+        K = K.flatten()
+        IV = IV.flatten()
+
+        return S, K, T, IV
+
+
 def main():
     # draw simple volatility smile
     import matplotlib
-    import matplotlib.pyplot as plt
 
     matplotlib.use("TkAgg")
 
-    sabr = SABR(0.1, 0.2, 0.1)
+    A = np.arange(0.01, 0.5, 0.1)  # len = 5
+    B = np.arange(0.1, 0.6, 0.1)  # len = 4
+    r = 0.1
+    # total graphs = 5 * 4 * 1 = 20
 
-    S = 100
-    T = np.linspace(0.1, 2, 10)
-    K = np.array([
-        np.random.randint(80, 120, 100) for _ in T
-    ])
+    fig = plt.figure(figsize=(8, 8))
+    plot_index = 1
 
-    IV = np.array([
-        [sabr.ivol(S, k, t) for k in K[i]] for i, t in enumerate(T)
-    ])
+    for i, a in enumerate(A):
+        for j, b in enumerate(B):
+            # Create a new subplot for each combination of A and B
+            ax = fig.add_subplot(len(A), len(B), plot_index, projection='3d')
+            ax.set_title(f"a={a:.1f}, b={b:.1f}, r={r:.1f}")
+            sabr = SABR(a, b, r)
+            # ignore pycharm warning
+            # noinspection PyTypeChecker
+            _, K, T, IV = sabr.preview(ax)
+            ax.plot_trisurf(K, T, IV, cmap='viridis')
+            plot_index += 1
 
-    # flatten K and IV, and make T the same size
-    T = np.array([
-        [t] * len(K[i]) for i, t in enumerate(T)
-    ]).flatten()
-
-    K = K.flatten()
-    IV = IV.flatten()
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_trisurf(K, T, IV, cmap='viridis')
-
-    ax.set_xlabel('Strike')
-    ax.set_ylabel('Maturity')
-    ax.set_zlabel('IV')
-
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
     plt.show()
 
 
